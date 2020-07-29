d6323db8e5d7648edeb41563eac93229b16bbedf
Evgeni Obukhovsky <eobuhovsky@gmail.com>
2020-07-30 01:31:35 +0200

    redirect

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+if (!empty($_SERVER['REMOTE_HOST']) && strpos($_SERVER['REMOTE_HOST'], '.herokuapp.com')) {
+    header('Location: http://www.ecommerce-consulting.pl/');
+    die();
+}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
